d10ba4fe560a855422278f51c0015d41f98ae439
Daniel Nägele <d.naegele@gridx.de>
2025-02-28 23:51:52 +0100

    fix(flake): adjust nesting of nixos module

--- flake.nix
+++ flake.nix
@@ -103,95 +103,95 @@
           export PATH=$GOPATH/bin:$PATH
         '';
       };
+    });
 
-      nixosModules.default = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: let
-        cfg = config.services.wunschkonzert;
-      in {
-        options.services.wunschkonzert = {
-          enable = lib.mkEnableOption "Enable wunschkonzert service";
-          environmentFile = lib.mkOption {
-            type = lib.types.nullOr lib.types.path;
-            description = ''
-              File path containing environment variables in the format of an EnvironmentFile. See {manpage}`systemd.exec(5)`.
-            '';
-            default = null;
-          };
-          server = {
-            name = lib.mkOption {
-              type = lib.types.str;
-              description = "Publicly reachable server address";
-            };
-            listen = lib.mkOption {
-              type = lib.types.str;
-              default = ":8080";
-              description = "Where the main app is listening";
-            };
-          };
-          auth = {
-            listen = lib.mkOption {
-              type = lib.types.str;
-              default = ":8081";
-              description = "Where the admin interface is listening";
-            };
-            client = {
-              id = lib.mkOption {
-                type = lib.types.str;
-                description = "OAuth Client ID for Spotify";
-              };
-              secret = lib.mkOption {
-                type = lib.types.str;
-                description = "OAuth Client Secret for Spotify";
-              };
-            };
-          };
-          playlist = lib.mkOption {
+    nixosModules.default = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: let
+      cfg = config.services.wunschkonzert;
+    in {
+      options.services.wunschkonzert = {
+        enable = lib.mkEnableOption "Enable wunschkonzert service";
+        environmentFile = lib.mkOption {
+          type = lib.types.nullOr lib.types.path;
+          description = ''
+            File path containing environment variables in the format of an EnvironmentFile. See {manpage}`systemd.exec(5)`.
+          '';
+          default = null;
+        };
+        server = {
+          name = lib.mkOption {
             type = lib.types.str;
-            description = "Playlist ID to add songs to";
+            description = "Publicly reachable server address";
           };
-          metrics.listen = lib.mkOption {
+          listen = lib.mkOption {
             type = lib.types.str;
-            default = ":9999";
-            description = "Where the app exposes its metrics";
-          };
-          verbose = lib.mkOption {
-            type = lib.types.bool;
-            default = false;
-            description = "Enable verbose logging";
+            default = ":8080";
+            description = "Where the main app is listening";
           };
         };
-
-        config = lib.mkIf cfg.enable {
-          systemd.services.wunschkonzert = {
-            description = "Wunschkonzert Service";
-            wantedBy = ["multi-user.target"];
-            after = ["network.target"];
-            serviceConfig = {
-              Type = "simple";
-              EnvironmentFile = config.services.wunschkonzert.environmentFile;
-              ExecStart = lib.concatStringsSep " \\\n " (
-                [
-                  "${self.packages.${pkgs.system}.default}/bin/server"
-                  "-server.name=${cfg.server.name}"
-                  "-server.listen=${cfg.server.listen}"
-                  "-auth.client.id=${cfg.auth.client.id}"
-                  "-auth.client.secret=${cfg.auth.client.secret}"
-                  "-auth.listen=${cfg.auth.listen}"
-                  "-playlist.id=${cfg.playlist}"
-                  "-metrics.listen=${cfg.metrics.listen}"
-                ]
-                ++ (lib.optional cfg.verbose "-verbose")
-              );
-              Restart = "always";
-              DynamicUser = "yes";
+        auth = {
+          listen = lib.mkOption {
+            type = lib.types.str;
+            default = ":8081";
+            description = "Where the admin interface is listening";
+          };
+          client = {
+            id = lib.mkOption {
+              type = lib.types.str;
+              description = "OAuth Client ID for Spotify";
             };
+            secret = lib.mkOption {
+              type = lib.types.str;
+              description = "OAuth Client Secret for Spotify";
+            };
+          };
+        };
+        playlist = lib.mkOption {
+          type = lib.types.str;
+          description = "Playlist ID to add songs to";
+        };
+        metrics.listen = lib.mkOption {
+          type = lib.types.str;
+          default = ":9999";
+          description = "Where the app exposes its metrics";
+        };
+        verbose = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Enable verbose logging";
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        systemd.services.wunschkonzert = {
+          description = "Wunschkonzert Service";
+          wantedBy = ["multi-user.target"];
+          after = ["network.target"];
+          serviceConfig = {
+            Type = "simple";
+            EnvironmentFile = config.services.wunschkonzert.environmentFile;
+            ExecStart = lib.concatStringsSep " \\\n " (
+              [
+                "${self.packages.${pkgs.system}.default}/bin/server"
+                "-server.name=${cfg.server.name}"
+                "-server.listen=${cfg.server.listen}"
+                "-auth.client.id=${cfg.auth.client.id}"
+                "-auth.client.secret=${cfg.auth.client.secret}"
+                "-auth.listen=${cfg.auth.listen}"
+                "-playlist.id=${cfg.playlist}"
+                "-metrics.listen=${cfg.metrics.listen}"
+              ]
+              ++ (lib.optional cfg.verbose "-verbose")
+            );
+            Restart = "always";
+            DynamicUser = "yes";
           };
         };
       };
-    });
+    };
   };
 }
